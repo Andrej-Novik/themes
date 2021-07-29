214ff0766f455338399eb669b71053f9ac72a8ce
Andrej-Novik <andrejnovikweb@gmail.com>
2021-07-29 11:46:52 +0300

    T-1: - change module file

--- src/store/modules/themes.js
+++ src/store/modules/themes.js
@@ -1,28 +1,24 @@
-export const state = () => ({
-  theme: "dark",
-});
-
-export const mutations = {
-  initDataTheme(state) {
-    const theme = localStorage.getItem("theme");
-    document.documentElement.dataset.theme = theme || "dark";
-    state.theme = theme;
-  },
-  setTheme(state, theme) {
-    localStorage.setItem("theme", theme);
-    document.documentElement.dataset.theme = theme;
-    state.theme = theme;
-  },
-  toggleTheme(state) {
-    let newTheme = state.theme === "dark" ? "light" : "dark";
-    localStorage.setItem("theme", newTheme);
-    document.documentElement.dataset.theme = newTheme;
-    state.theme = newTheme;
-  },
-};
-
 export default {
   namespaced: true,
-  state,
-  mutations,
+  state: {
+    theme: "dark",
+  },
+  mutations: {
+    initDataTheme(state) {
+      const theme = localStorage.getItem("theme");
+      document.documentElement.dataset.theme = theme || "dark";
+      state.theme = theme;
+    },
+    setTheme(state, theme) {
+      localStorage.setItem("theme", theme);
+      document.documentElement.dataset.theme = theme;
+      state.theme = theme;
+    },
+    toggleTheme(state) {
+      let newTheme = state.theme === "dark" ? "light" : "dark";
+      localStorage.setItem("theme", newTheme);
+      document.documentElement.dataset.theme = newTheme;
+      state.theme = newTheme;
+    },
+  },
 };
